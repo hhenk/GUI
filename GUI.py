3273b78435dafe04b0e663267c033dfc5e8a7f52
hhenk <barrystrengholt@gmail.com>
2016-04-14 12:02:25 +0200

    GUI.py
    Tutorial for the stuff

---
+++ GUI.py
@@ -0,0 +1,22 @@
+#Gui tutorial 1
+
+import tkinder
+
+class Example(Frame):
+	def __init__(self,parent):
+		Frame.__init__(self, parent, background="white")
+		self.parent=parent
+		self.initUI()
+		
+	def initUI(self):
+		self.parent.title("Simple")
+		selft.pack(fill=BOTH, expand=1)
+		
+def main():
+	root = Tk()
+	root.geometry("250x150+300+300")
+	app = Example(root)
+	root.mainloop()
+	
+if __name__ == '__main__':
+	main()
